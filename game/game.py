f09403dc4c89db1932c9b9b978ecbd7fe6818eb0
zuka <157312286+zukadzm@users.noreply.github.com>
2024-03-10 13:18:58 -0700

    Add files via upload

---
+++ game/game.py
@@ -0,0 +1,23 @@
+#register
+name = input("Please enter your name: ")
+password = input("Please enter your password: ")
+repeat_password = input("Please enter your password again: ")
+
+if password == repeat_password:
+        print(name, "registered succsesfully!")
+else:
+    print("Invalid Passwords!")
+#game time
+    
+number= 13
+
+
+number = int(input("guess number 1 to 20: "))
+
+if number == 13:
+      print("correct number")
+
+else:
+      print("wrong")
+      
+# end game
